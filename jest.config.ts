08cd3561335715402f741aebe9dd94c55e45dd6b
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-01-18 16:27:34 +0300

    add different configs (docker/jest/eslint)

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+import 'jest-ts-auto-mock'
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+    verbose: true,
+    bail: 15,
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        ".(ts|tsx)": "ts-jest",
+        "^.+\\.(t|j)s$": "ts-jest"
+    },
+    globals: {
+        "testPathIgnorePatterns ": [
+            "/.cache.ti.api.gs.ru.shares.report/",
+            "/assets/",
+            "/benchmark/",
+            "/dist/",
+            "/node_modules/"
+        ],
+        "testMatch": [
+            "*.(test|spec).(ts|js)"
+        ],
+        "ts-jest": {
+            "tsconfig": "tsconfig.json",
+            "compiler": "ttypescript",
+        }
+    }
+};
+export default config;
