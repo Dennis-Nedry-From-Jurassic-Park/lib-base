b31461fe1aee8a24e0a405309500b0ce2a63dcf8
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-01-19 15:29:21 +0300

    update secrets

--- src/utility-methods/env.ts
+++ src/utility-methods/env.ts
@@ -8,10 +8,25 @@ const rootDir = getAppRootDir();
 dotenv.config({ path: rootDir+'/.env', debug: true });
 
 const secrets = {
+	sheet: process.env.SHEET,
+	scopes: process.env.SCOPES,
+
 	telegramToken: process.env.TELEGRAM_TOKEN,
 	telegramInvestingBotId: process.env.TELEGRAM_INVESTING_BOT_ID,
 
-	finnhubKeyApi: process.env.FINNHUB_KEY_API
+	token: process.env.TOKEN,
+
+	brokerAccountId: process.env.BROKER_ACCOUNT_ID_BROKERAGE,
+	brokerAccountIdIis: process.env.BROKER_ACCOUNT_ID_IIS,
+
+	googleSpreadsheetIisB2: process.env.IIS_B2,
+	googleSpreadsheetBrokerageB2: process.env.BROKERAGE_B2,
+
+	finnhubKeyApi: process.env.FINNHUB_KEY_API,
+
+	redisHost: process.env.REDIS_HOST,
+	redisPort: process.env.REDIS_PORT,
+	redisPassword: process.env.REDIS_PASSWORD
 };
 
 export default secrets;
