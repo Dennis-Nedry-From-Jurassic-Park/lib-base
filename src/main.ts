bc35b8e126bd5f45dc90949765825010d450eb70
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-03-23 17:49:09 +0300

    *

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,23 @@ import {logger} from "./logger/pino";
 console.log(DAY);
 
 logger.info('1111')
+
+let obj: any[] = []
+
+for(;;) {
+    let new_obj = {
+        a: '1',
+        b: '2',
+        c: {
+            x: '42'
+        }
+    }
+    obj.push(new_obj)
+    if(JSON.stringify(obj).length * 2 > 64000){
+        obj.pop()
+        // TODO: send obj to mq ycloud
+        obj = []
+        obj.push(new_obj)
+        break;
+    }
+}
