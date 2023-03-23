b3f7ccd8d937b511f604427ac3f1c3f0be1c0349
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-03-23 21:16:25 +0300

    utc methods

--- src/datetime/dt.ts
+++ src/datetime/dt.ts
@@ -1,10 +1,21 @@
 import {addDays} from 'date-fns'
-var format = require('date-fns/format')
+
+const format = require('date-fns/format');
+const { utcToZonedTime } = require('date-fns-tz')
+
+import { UTCDate } from "@date-fns/utc";
+
 
 export const add_days = async (datetime: string, days: number) => {
     const dt = new Date(datetime);
     return addDays(dt, days);
 }
+export const zonedDate = () => utcToZonedTime(new Date().toUTCString(), 'Europe/London')
+export const getUTCDate = (date = new Date()) => {
+    return new Date(date.getTime() + date.getTimezoneOffset()*1*1000)
+}
 
-export const now_utc = () => format(new Date(), "yyyy-MM-dd hh:mm:ss.SSS")
+export const now_utc1 = () => format(utcToZonedTime(new Date(), 'UTC'), 'yyyy-MM-dd HH:mm:ss')
+export const now_utc = () => new UTCDate().toISOString().slice(0, -1).replace('T', ' ')
+export const now_utc0 = () => format(new Date(), "yyyy-MM-dd HH:mm:ss.SSS", { timeZone: 'Europe/London' })
 export const now_iso = () => format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,43 @@
-import {DAY} from "./constants/date.time.formats";
-import {logger} from "./logger/pino";
+import {getUTCDate, now_utc, now_utc1} from "./datetime/dt";
 
-console.log(DAY);
 
-logger.info('1111')
+console.log(getUTCDate())
+console.log(now_utc())
+
+
+const {format,zonedTimeToUtc,utcToZonedTime} = require("date-fns-tz");
+const today = new Date(); // Wed Sep 16 2020 13:25:16
+const timeZone = 'UTC'; // Let's see what time it is Down Under
+const timeInBrisbane = zonedTimeToUtc(today, timeZone);
+console.log(`
+Default time zone: ${format(today, 'yyyy-MM-dd HH:mm:ss')}
+Time in Paris: ${format(timeInBrisbane, 'yyyy-MM-dd HH:mm:ss')
+}`);
+
+const timeInBrisbane2 = utcToZonedTime(today, timeZone);
+console.log(`
+Default time zone: ${format(today, 'yyyy-MM-dd HH:mm:ss')}
+Time in Paris: ${format(timeInBrisbane2, 'yyyy-MM-dd HH:mm:ss')
+}`);
+
+console.log(now_utc1());
 
 let obj: any[] = []
 
-for(;;) {
-    let new_obj = {
-        a: '1',
-        b: '2',
-        c: {
-            x: '42'
-        }
-    }
-    obj.push(new_obj)
-    if(JSON.stringify(obj).length * 2 > 64000){
-        obj.pop()
-        // TODO: send obj to mq ycloud
-        obj = []
-        obj.push(new_obj)
-        break;
-    }
-}
+// for(;;) {
+//     let new_obj = {
+//         a: '1',
+//         b: '2',
+//         c: {
+//             x: '42'
+//         }
+//     }
+//     obj.push(new_obj)
+//     if(JSON.stringify(obj).length * 2 > 64000){
+//         obj.pop()
+//         // TODO: send obj to mq ycloud
+//         obj = []
+//         obj.push(new_obj)
+//         break;
+//     }
+// }
