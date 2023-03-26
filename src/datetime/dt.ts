5c26be198d781afb04471b1cce26093b277f2861
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-03-26 16:39:45 +0300

    date-fns-tz deps

--- src/datetime/dt.ts
+++ src/datetime/dt.ts
@@ -8,6 +8,6 @@ export const add_days = async (datetime: string, days: number) => {
     return addDays(dt, days);
 }
 
-export const now_utc = () => format(utcToZonedTime(new Date(), 'UTC'), 'yyyy-MM-dd HH:mm:ss', { timeZone: 'UTC' })
+export const now_utc = () => format(utcToZonedTime(new Date()), 'yyyy-MM-dd HH:mm:ss.SSS', { timeZone: 'UTC' })
 export const now_utc0 = () => format(new Date(), "yyyy-MM-dd HH:mm:ss.SSS", { timeZone: 'Europe/London' })
 export const now_iso = () => format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
