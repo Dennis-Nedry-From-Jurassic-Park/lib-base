8dd4a08c6b6c97cb11fbc877c3bd161469360c10
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-12-28 07:15:56 +0300

    splitToChunks

---
+++ src/string/chunks.ts
@@ -0,0 +1,7 @@
+// export const splitToChunks_ = (arr, n) => {
+//     return arr.length ? [arr.slice(0, n), ...splitToChunks_(arr.slice(n), n)] : [];
+// };
+//
+// export function* splitToChunks__ (str, size) {
+//     for(let i = 0; i < str.length; i+= size ) yield str.slice(i, i + size);
+// }
