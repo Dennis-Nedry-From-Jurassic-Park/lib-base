c6c0e357bea1a5a29c51bda568c0f4a5fb148270
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2023-12-28 07:14:15 +0300

    add getTimestampInSeconds

--- src/datetime/dt.ts
+++ src/datetime/dt.ts
@@ -11,3 +11,7 @@ export const add_days = async (datetime: string, days: number) => {
 export const now_utc = () => format(utcToZonedTime(new Date()), 'yyyy-MM-dd HH:mm:ss.SSS', { timeZone: 'UTC' })
 export const now_utc0 = () => format(new Date(), "yyyy-MM-dd HH:mm:ss.SSS", { timeZone: 'Europe/London' })
 export const now_iso = () => format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+
+export const getTimestampInSeconds = () => {
+    return Math.floor(Date.now() / 1000)
+}
