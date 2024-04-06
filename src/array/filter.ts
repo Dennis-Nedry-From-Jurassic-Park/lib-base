ed3c142c34f70e2291487ebe025e38c573e482f7
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2024-04-06 10:38:47 +0300

    add intersection

--- src/array/filter.ts
+++ src/array/filter.ts
@@ -2,16 +2,16 @@ export function genericFilter<T>(
     objects: T[],
     properties: Array<keyof T>,
     queries: Array<string>[] | Array<number>[]
-):T[] {
-    return objects.filter((object) =>  {
+): T[] {
+    return objects.filter((object) => {
         let count = 0;
         properties.some((props) => {
             const objectValue = object[props]
-            if(typeof objectValue === "string" || typeof objectValue === "number") {
+            if (typeof objectValue === "string" || typeof objectValue === "number") {
                 queries.forEach((query) => {
                     query.forEach((queryValue) => {
-                        if(queryValue === objectValue) {
-                            count+=1;
+                        if (queryValue === objectValue) {
+                            count += 1;
                         }
                     })
                 })
@@ -19,4 +19,11 @@ export function genericFilter<T>(
         })
         return count === properties.length;
     })
+}
+
+export const intersection = async (
+    arr1: any[],
+    arr2: any[],
+) => {
+    return arr1.filter(x => arr2.includes(x));
 }
